d1b6343abd48bed54c19336ba9e06d6b98e949b9
jeongseonwoo <dydry155@naver.com>
2016-06-14 17:11:21 +0900

    Simple_GAME(Homework)
    Just moving Cube

---
+++ asdd/Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float speed = 30.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveDir = new Vector3 (Input.GetAxis("Horizontal") , 0, Input.GetAxis("Vertical"));
+        moveDir = transform.TransformDirection (moveDir);
+        moveDir *= Time.deltaTime * 30.0f;
+        moveDir += transform.position;
+
+        transform.position = moveDir;
+
+	}
+}
